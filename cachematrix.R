ce5910eaeb3a0664bf837c8e958169ab2db3dcd1
gabrielmariante <gabrielmariante@outlook.com>
2015-03-22 14:28:18 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix stores a matrix and its inverse on a list.
+##
+## cacheSolve searches the matrix in the cache created by makeCacheMatrix and
+## returns the stored inverse if it finds it. If it does not find it, the program
+## will calculate and return the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv_x = x^-1
+  cache = list(x,inv_x)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  cache = makeCacheMatrix(x)
+  len = length(cache)
+  achou = 0
+  
+  for (i in 1:len){
+    if (cache[[i]] == x){
+      inv = cache[[i+1]]
+      achou = 1
+      break
+    }else
+      i = i+2
+  }
+  
+  if (achou == 0){
+    inv = x^-1    
+  }
+
+  inv
 }
